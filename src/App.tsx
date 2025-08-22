ae5592cf67be067b354cdd4de0d7210dcd980768
GATEWAYGROUP\Ziyauddin.Ansari <ziyauddin.ansari@thegatewaydigital.com>
2025-08-22 17:47:43 +0530

    Add gh-pages support, update routing to HashRouter, and improve CSS formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import ShipsList from "./components/ShipsList/ShipsList";
 import ShipDetail from "./components/ShipDetail/ShipDetail";
--- src/components/ShipsList/ShipsList.tsx
+++ src/components/ShipsList/ShipsList.tsx
@@ -69,6 +69,7 @@ const ShipsList = () => {
           Next
         </button>
       </div>
+      </div>
     </div>
   );
 };
